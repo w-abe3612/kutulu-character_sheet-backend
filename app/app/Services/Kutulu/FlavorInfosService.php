119d439e2cd1839e45e14cd60f5daee37d337217
w-abe3612 <ookanadamo3612@gmail.com>
2023-07-25 21:40:38 +0900

    ビジネスロジックをServiceフォルダに

---
+++ app/app/Services/Kutulu/FlavorInfosService.php
@@ -0,0 +1,132 @@
+<?php
+ 
+namespace App\Services\Kutulu;
+ 
+/**
+ * 
+ */
+class FlavorInfosService
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Request $request)
+    {
+        $result = [];
+        $result = FlavorInfos::where('character_info_id', $request->character_id)
+                    ->where('user_id', Auth::id() )
+                    ->get();
+        return response()->json($result, 201);
+/*
+        return $result
+            ? response()->json($result, 201)
+            : response()->json([], 501);
+*/
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function view(Request $request)
+    {
+        $result = [];
+
+        // todo 一度で取得できる感じのロジックにする
+        $users = DB::table('users')
+            ->select('id as user_id')
+            ->where('public_page_token', $request->userPageToken )
+            ->first();
+        
+        $characterInfo = DB::table('character_infos')
+            ->select('id as character_id')
+            ->where('user_id', $users->user_id)
+            ->where('public_page_token', $request->characterPageToken)
+            ->where('delete_flg', '!=', true)
+            ->first();
+
+        if ( !empty($users) && !empty($characterInfo) ) {
+            $result = FlavorInfos::where('character_info_id', $characterInfo->character_id)
+                    ->where('user_id', $users->user_id )
+                    ->get();
+        }
+
+        return $result
+            ? response()->json($result, 201)
+            : response()->json([], 500);
+    }
+
+    /**
+     * FlavorInfoを作成するコントローラー
+     * リクエスト経由ではなく、CharacterSheetControllerから呼び出される
+     *
+     * @param  int  $id
+     * @return 
+     */
+    public function create($create_request,$character_id)
+    {
+        $result = '';
+        $infos = [];
+        
+        if( !empty($create_request) && !empty($character_id) ) {
+            $infos = $create_request;
+
+            foreach($create_request as $key => $val){
+                $infos[$key]['user_id'] = Auth::id();
+                $infos[$key]['character_info_id'] = $character_id;
+                $infos[$key]['flavor_info_value'] = !empty( $infos[$key]['flavor_info_value'] )
+                                                        ? $infos[$key]['flavor_info_value']: '';
+            }
+            $result = FlavorInfos::insert($infos);
+        }
+ 
+        return $result;
+    }
+
+    /**
+     * FlavorInfoを更新するコントローラー
+     * リクエスト経由ではなく、CharacterSheetControllerから呼び出される
+     *
+     * @param  int  $id
+     * @return 
+     */
+    public function edit($update_request,$character_id)
+    {
+        $result = '';
+        $update_values = [];
+
+        $old_flavor_info = 
+            FlavorInfos::where('character_info_id', $character_id)
+                        ->where('user_id', Auth::id() )
+                        ->get();
+
+        if ( !empty( $update_request ) && !empty( $character_id ) ) {
+            foreach ( $update_request as $request_value) {
+                foreach( $old_flavor_info as $old_value  ) {
+                    if( $old_value['flavor_info_param'] === $request_value['flavor_info_param'] ) {
+                        $result_values[] = [
+                            'id' => !empty($old_value['id'])? $old_value['id']: '',
+                            'user_id' => Auth::id(),
+                            'character_info_id' => $character_id,
+                            'flavor_info_name'  => $request_value['flavor_info_name'] ,
+                            'flavor_info_order' => $request_value['flavor_info_order'] ,
+                            'flavor_info_param' => $request_value['flavor_info_param'] ,
+                            'flavor_info_value' => $request_value['flavor_info_value'] ,
+                        ];
+                    }
+                }
+            }
+            
+            $result = FlavorInfos::upsert( $result_values, 
+                    ['id','user_id','character_info_id'], 
+                    ['flavor_info_name' , 'flavor_info_order', 'flavor_info_param','flavor_info_value']);
+        }
+
+        return $result;
+    }
+}
---
+++ app/app/Services/Kutulu/KutuluInfoService.php
@@ -0,0 +1,109 @@
+<?php
+ 
+namespace App\Services\Kutulu;
+ 
+/**
+ * 
+ */
+class KutuluInfoService
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Request $request)
+    {
+        $result = [];
+        $result = KutuluInfo::where('character_info_id', $request->character_id)
+                    ->where('user_id', Auth::id() )
+                    ->get();
+
+        return $result
+            ? response()->json($result, 201)
+            : response()->json([], 501);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function view(Request $request)
+    {
+        $result = [];
+
+        // todo 一度で取得できる感じのロジックにする
+        $users = DB::table('users')
+            ->select('id as user_id')
+            ->where('public_page_token', $request->userPageToken )
+            ->first();
+        
+        $characterInfo = DB::table('character_infos')
+            ->select('id as character_id')
+            ->where('user_id', $users->user_id)
+            ->where('public_page_token', $request->characterPageToken)
+            ->where('delete_flg', '!=', true)
+            ->first();
+
+        if ( !empty($users) && !empty($characterInfo) ) {
+            $result = KutuluInfo::where('character_info_id', $characterInfo->character_id)
+                ->where('user_id', $users->user_id )
+                ->get();
+        }
+
+        return $result
+            ? response()->json($result, 201)
+            : response()->json([], 500);
+    }
+
+    /**
+     * KutuluInfoを作成するコントローラー
+     * リクエスト経由ではなく、CharacterSheetControllerから呼び出される
+     *
+     * @param  int  $id
+     * @return 
+     */
+    public function create($create_request = null,$character_id = null)
+    {
+        $result = '';
+        $infos = [];
+        if ( !empty( $create_request ) && !empty( $character_id ) ) {
+            $infos = $create_request;
+            $infos['user_id'] = Auth::id();
+            $infos['character_info_id'] = $character_id;
+
+            $result = KutuluInfo::create( $infos );
+        }
+
+        return $result;
+    }
+
+    /**
+     * KutuluInfoを更新するコントローラー
+     * リクエスト経由ではなく、CharacterSheetControllerから呼び出される
+     *
+     * @param  int  $id
+     * @return 
+     */
+    public function edit($update_request = null, $character_id = null )
+    {
+        $result = '';
+        if ( !empty( $update_request ) && !empty( $character_id ) ) {
+            $kutuluInfo = KutuluInfo::where('character_info_id', $character_id )
+                        ->where('user_id', Auth::id() )
+                        ->first();
+
+            $kutuluInfo->character_title      = $update_request['character_title'];
+            $kutuluInfo->injury_value         = $update_request['injury_value'];
+            $kutuluInfo->possession_item      = !empty( $update_request['possession_item'] )? $update_request['possession_item']: '';
+            $kutuluInfo->character_preference = !empty( $update_request['character_preference'] )? $update_request['possession_item']: '';
+
+            $result = $kutuluInfo->update();
+        }
+
+        return $result;
+    }
+}
